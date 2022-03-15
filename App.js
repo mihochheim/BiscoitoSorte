3ffdb9cf2f2253fbc5940f22f1047f85d9443da8
Milena Hochheim <milenakhochheim@gmail.com>
2022-03-15 00:46:51 -0300

    DONE

--- App.js
+++ App.js
@@ -1,11 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
-export default function App() {
+function App() {
+  const [img, setImg] = useState(require('./src/biscoito.png'));
+  const [textoFrase, setTextoFrase] = useState('');
+
+  let frases = [
+    'Siga os bons e aprenda com eles.', 
+    'O bom-senso vale mais do que muito conhecimento.', 
+    'O riso é a menor distância entre duas pessoas.', 
+    'Deixe de lado as preocupações e seja feliz.',
+    'Realize o óbvio, pense no improvável e conquiste o impossível.',
+    'Acredite em milagres, mas não dependa deles.',
+    'A maior barreira para o sucesso é o medo do fracasso.'
+  ];
+
+  function quebraBiscoito(){
+    let random = Math.floor(Math.random()*frases.length)
+    //console.log(frases[random]);
+
+    setTextoFrase('"' + frases[random] + '"');
+
+    setImg(require('./src/biscoitoAberto.png'))
+  }
+
+  function reiniciar(){
+    setImg(require('./src/biscoito.png'));
+    setTextoFrase('');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={img}
+        style={styles.img}
+      />
+
+      <Text style={styles.frase}>{textoFrase}</Text>
+      <TouchableOpacity style={styles.botao} onPress ={quebraBiscoito}>
+        <View style={styles.btnArea}>
+          <Text style={styles.texto}>
+            Quebrar biscoito
+          </Text>
+        </View>
+      </TouchableOpacity>
+
+      
+      <TouchableOpacity style={[styles.botao, { marginTop: 15, borderColor: '#121212' }]} onPress={reiniciar}>
+        <View style={styles.btnArea}>
+          <Text style={[styles.texto, { color: '#121212' }]}>
+            Reiniciar biscoito
+          </Text>
+        </View>
+      </TouchableOpacity>
+
     </View>
   );
 }
@@ -13,8 +61,45 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
   },
-});
+
+  img: {
+    width: 230,
+    height: 230,
+  },
+
+  frase: {
+    fontSize: 20,
+    color: '#dd7b22',
+    margin: 30,
+    fontStyle: 'italic',
+    textAlign: 'center'
+  },
+
+  botao: {
+    width: 230,
+    height: 50,
+    borderColor: '#dd7b22',
+    borderWidth: 2,
+    borderRadius: 25
+  },
+
+  btnArea: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+
+  },
+
+  texto: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#dd7b22',
+
+  }
+
+
+})
+export default App;
